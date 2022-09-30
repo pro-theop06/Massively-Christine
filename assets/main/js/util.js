ac8db845d83addd7feae96d5f7bbf51418c1ffa1
Théo <github@theopeltier.me>
2022-09-30 11:47:00 +0200

    :bug: fix: js violation in event listener 'touchmove'

--- assets/main/js/util.js
+++ assets/main/js/util.js
@@ -242,7 +242,6 @@
 						if (($this.scrollTop() < 0 && diffY < 0)
 						|| (ts > (th - 2) && ts < (th + 2) && diffY > 0)) {
 
-							event.preventDefault();
 							event.stopPropagation();
 
 						}
